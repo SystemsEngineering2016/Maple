d56b0ce079ff9e972862acf11215ea83cb8b339f
Pius <piusjude@Pius.local>
2017-02-28 22:25:26 +0000

    able to create and display versions for a system

---
+++ maple_project/app/controllers/versions_controller.rb
@@ -0,0 +1,12 @@
+class VersionsController < ApplicationController
+  def create
+    @system = System.find(params[:system_id])
+    @version = @system.versions.create(version_params)
+    redirect_to system_path(@system)
+  end
+
+  private
+    def version_params
+      params.require(:version).permit(:Version_Name)
+    end
+end
