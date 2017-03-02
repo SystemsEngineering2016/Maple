9105bb1c93449d334bd3325bdf2289ed1b64c1f8
Matt Policane <mattpolicane@Matts-MacBook-Pro.local>
2017-03-02 00:45:40 +0000

    Tidied up a bit

--- maple_project/app/controllers/systems_controller.rb
+++ maple_project/app/controllers/systems_controller.rb
@@ -14,7 +14,10 @@ end
   def edit
     @system = System.find(params[:id])
   end
+  def version_view
+    @systems = System.all
 
+  end
   def create
     @system = System.new(system_params)
 
@@ -48,8 +51,7 @@ end
     params.require(:system).permit(:System_Name)
   end
 
-  def version_view
-  end
+
   def create_version
     @systems = System.all
   end
