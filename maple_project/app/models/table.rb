4a9e58973d545e32d6e415262038f027a7bc3f2e
Pius <piusjude@Pius.local>
2017-03-29 17:08:43 +0100

    created more error validation: for uniqueness of logical name and integer check for display width and height

--- maple_project/app/models/table.rb
+++ maple_project/app/models/table.rb
@@ -1,4 +1,7 @@
 class Table < ApplicationRecord
   belongs_to :version
   validates_presence_of :Longest_Name, :message => "must be provided. Please try again. "
+  validates_uniqueness_of :Longest_Name, :scope => :version_id, :message => "already exists. Please choose a new logical name for data point. (Logical names are not case sensitive).",:case_sensitive => false
+  validates :Display_Width, numericality: { only_integer: true,:allow_blank => true, :message => "must be an integer. Please try again." }
+  validates :Display_Height, numericality: { only_integer: true, :allow_blank => true, :message => "must be an integer. Please try again." }
 end
