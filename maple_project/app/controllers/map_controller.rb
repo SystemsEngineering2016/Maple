da56549d178afc9bf438c8285a813bacaa880e25
Pius <piusjude@Pius.local>
2017-04-04 11:10:10 +0100

    moved files in map/index to map/new and can now submit via a form in new.html, the mapfrom and mapto id

--- maple_project/app/controllers/map_controller.rb
+++ maple_project/app/controllers/map_controller.rb
@@ -1,8 +1,23 @@
 class MapController < ApplicationController
 
-
-  def index
-
+def index
     @systems = System.all
   end
+
+  def new
+    @systems = System.all
+    @map = Map.new
+  end
+
+  def create
+    @map = Map.new(map_params)
+
+    @map.save
+    redirect_to map_index_path
+  end
+
+  private
+    def map_params
+      params.require(:map).permit(:mapfrom_id, :mapto_id)
+    end
 end
