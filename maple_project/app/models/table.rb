1fa4e6f60800c0067be3e3f4170457ee4d4c0a6b
Pius <piusjude@Pius.local>
2017-04-04 22:20:09 +0100

    when u delete a datapoint, the associated datapoint mappings will be deleted

--- maple_project/app/models/table.rb
+++ maple_project/app/models/table.rb
@@ -4,6 +4,6 @@ class Table < ApplicationRecord
   validates_uniqueness_of :Logical_Name, :scope => :version_id, :message => "already exists. Please choose a new logical name for data point. (Logical names are not case sensitive).",:case_sensitive => false
   validates :Display_Width, numericality: { only_integer: true,:allow_blank => true, :message => "must be an integer. Please try again." }
   validates :Display_Height, numericality: { only_integer: true, :allow_blank => true, :message => "must be an integer. Please try again." }
-  has_many :mapfrom_mappings, :class_name => 'Map', :foreign_key => 'mapfrom_id'
-  has_many :mapto_mappings, :class_name => 'Map', :foreign_key => 'mapto_id'
+  has_many :mapfrom_mappings, :class_name => 'Map', :foreign_key => 'mapfrom_id', dependent: :destroy
+  has_many :mapto_mappings, :class_name => 'Map', :foreign_key => 'mapto_id', dependent: :destroy
 end
